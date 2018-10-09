d4ef26fd29cc9e25836ba23afe8709c4e29cc5b4
CCY <chenchunyang@chiyue365.com>
2018-10-09 11:34:12 +0800

    新建项目

---
+++ CYWeChat/CYWeChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CYWeChat
+//
+//  Created by ccy on 2018/10/9.
+//  Copyright © 2018年 ccy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CYWeChat/CYWeChat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CYWeChat
+//
+//  Created by ccy on 2018/10/9.
+//  Copyright © 2018年 ccy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
